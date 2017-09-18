d95d5c68284f56442ca33c3eb148054e4df06f53
Rosalie Orth <30411575+rjorth@users.noreply.github.com>
2017-09-18 10:17:14 -0400

    Update p1.py

--- p1.py
+++ p1.py
@@ -1,8 +1,33 @@
 # 2.5.6
-number1 = int(raw_input('Enter First number : '))
-number2 = int(raw_input('Enter Second number : '))
-number3 = int(raw_input('Enter Third number : '))
-number4 = int(raw_input('Enter Fourth number: '))
+while True:
+    try:
+        number1 = int(raw_input('Enter First number : '))
+    except ValueError:
+        print("Your input is not a number")
+        continue
+    else:
+        break
+    try:
+        number2 = int(raw_input('Enter Second number : '))
+    except ValueError:
+        print("Your input is not a number")
+        continue
+    else:
+        break
+    try:
+        number3 = int(raw_input('Enter Third number : '))
+    except ValueError:
+        print("Your input is not a number")
+        continue
+    else: 
+        break
+    try:
+        number4 = int(raw_input('Enter Fourth number: '))
+    except ValueError:
+        print("Your input is not a number")
+        continue
+    else:
+        break
 def smallest(num1, num2, num3, num4):
     if (num1 < num2) and (num1 < num3) and (num1 < num4):
         smallest_num = num1
@@ -14,4 +39,3 @@ def smallest(num1, num2, num3, num4):
       		smallest_num = num4
     print('The smallest of the numbers is : ', smallest_num)
 smallest(number1, number2, number3, number4)
-#still need to create the prompt for 'invalid input' that does not disrupt the program. 
