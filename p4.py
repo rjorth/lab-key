a5c86b1e226dfdf93b248652b158078a36425c23
Rosalie Orth <30411575+rjorth@users.noreply.github.com>
2017-09-18 11:02:46 -0400

    Create p4.py

---
+++ p4.py
@@ -0,0 +1,11 @@
+import getpass
+getpass.getpass("Player1, please enter your number")
+
+
+for i in range(0,20):
+    player2 = raw_input("Player2, please input your guess")
+    if player2 !== getpass:
+        print("Try again")
+        continue
+    else: 
+        print("You are right") 
