5fad0a32bad3adc7cec4e3a44f6fabda6b0a013f
Rosalie Orth <30411575+rjorth@users.noreply.github.com>
2017-09-19 10:36:58 -0400

    Update p4.py

--- p4.py
+++ p4.py
@@ -9,3 +9,18 @@ for i in range(0,20):
         continue
     else: 
         print("You are right") 
+# alternative attempt
+import getpass
+player1 = getpass.getpass('Player1, please enter your number: ')
+
+for player2 in range(0,5):
+    player2 = raw_input('Player 2, please input your guess')
+    if player2 == player1:
+        print('You are correct!')
+        break
+    elif player2 != player1 :
+        print('You are not correct.')
+        continue
+    else:
+        print('You are correct after 5 guesses.')
+        break
