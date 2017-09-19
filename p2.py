52e15c3a7ee393aa2ae5452373cd470b157104c1
Rosalie Orth <30411575+rjorth@users.noreply.github.com>
2017-09-19 10:11:06 -0400

    Update p2.py

--- p2.py
+++ p2.py
@@ -1,8 +1,9 @@
-str = raw_input("Please enter a string:")
-n = int(raw_input("Please enter a number:"))
-
-while n >=0:
-  print (str * int(n)) 
-  n -=1
+#needed edits: program ends when -1 is entered. 
+while True:
+    str = raw_input("Please enter a string:")
+    n = int(raw_input("Please enter a number:"))
+    while n > 0:
+        print (str * int(n))
+        n -= 1
     
-#program must end if -1 is entered. program must continue running until ended. 
+
