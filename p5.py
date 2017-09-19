4ae035ca58d878d567443f94c40c45a9cf9d8b86
Rosalie Orth <30411575+rjorth@users.noreply.github.com>
2017-09-19 10:25:40 -0400

    Update p5.py

--- p5.py
+++ p5.py
@@ -1,11 +1,10 @@
-word = raw_input("Please input a palindrome")
+#this problem is completed!
+while True:
+    word = raw_input('Please input a palindrome')
+    word_rev = reversed(word)
+    if list(word) == list(word_rev):
+        print('Welcome to the Wonderland!')
+        break
+    else:
+        print('No, you must input a palindrome')
 
-word_rev = reversed(word)
-
-def is_palindrome(word):
-  if list(word) == list(word_rev):
-    print'True, it is a palindrome'
-  else:
-    print'No, you must input a palindrome'
-
-is_palindrome(word)
